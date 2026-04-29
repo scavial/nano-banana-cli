046c36a550a822823fe10d59eeaee5136ce6aa45
Sca Vial <scavial@users.noreply.github.com>
2026-04-30 02:19:31 +0800

    fix: print error message to stderr before exiting

--- cmd/nanobanana/main.go
+++ cmd/nanobanana/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/lyalindotcom/nano-banana-cli/internal/cli"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	if err := cli.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
